9ada579bf13b252678ce2020cff11f40439397e3
vladilexa1 <vlad.slip4enko2012@gmail.com>
2025-05-02 19:34:56 +0300

    feat: add function for save photo

--- PhotoMaker.py
+++ PhotoMaker.py
@@ -1,14 +1,22 @@
 import time
+import cv2
+from datetime import datetime
 from picamera2 import Picamera2
 
 
 def make_photo(pcam2: Picamera2):
     pcam2.start()
-    time.sleep(2)
+    time.sleep(2) # wait load camera
     img = pcam2.capture_array()
     pcam2.stop()
     pcam2.close()
     return img
+
+def save_photo(img, name=""):
+    if name == "":
+        name = datetime.now().time()
+    cv2.imwrite(f"./PhotoMaker/{name}.jpg", img)
+
 
 if __name__ == "__main__":
     picam2 = Picamera2()
