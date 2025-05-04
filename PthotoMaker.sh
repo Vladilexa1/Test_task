adba07e159e0a3688ab4802262d52b6c6c9cc092
Vladyslav Slipchenko <124776328+Vladilexa1@users.noreply.github.com>
2025-05-04 14:26:43 +0300

    feat: install git

--- PthotoMaker.sh
+++ PthotoMaker.sh
@@ -2,5 +2,6 @@ echo 'gpu mem=128' | sudo tee -a /boot/firmware/config.txt
 echo 'dtoverlay=imx219' | sudo tee -a /boot/firmware/config.txt
 sudo apt install python3-opencv -y
 sudo apt install python3-picamera2 -y
+sudo apt install git -y
 git clone https://github.com/Vladilexa1/Test_task.git
 mkdir ./Test_task/img
